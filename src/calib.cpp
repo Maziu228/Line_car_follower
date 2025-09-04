b915f00d018f9ac10bc74dbacb03bbfdb3c7e455
jonga <jgmoegster@protonmail.com>
2025-09-04 13:28:01 +0200

    Added calibration

---
+++ src/calib.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jonga on 04.09.2025.
+//
+
+#include "calib.hpp"
---
+++ src/calib.hpp
@@ -0,0 +1,13 @@
+//
+// Created by jonga on 04.09.2025.
+//
+
+#ifndef LINE_CAR_FOLLOWER_CALIB_HPP
+#define LINE_CAR_FOLLOWER_CALIB_HPP
+
+
+class calib {
+};
+
+
+#endif //LINE_CAR_FOLLOWER_CALIB_HPP
